69080fccf1c993745bbb5e04d2c7ec2879b2ad4c
quentinikeno <ikenoq@gmail.com>
2022-09-07 17:55:16 -1000

    Add tests for pop.

--- double-linked-list.test.js
+++ double-linked-list.test.js
@@ -57,7 +57,7 @@ describe("pop", function () {
 		expect(lst.pop()).toBe(10);
 		expect(lst.head.val).toBe(5);
 		expect(lst.tail.val).toBe(5);
-		expect(lst.head.next.val).toBe(null);
+		expect(lst.head.next).toBe(null);
 		expect(lst.length).toBe(1);
 
 		expect(lst.pop()).toBe(5);
